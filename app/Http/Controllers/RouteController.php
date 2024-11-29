8293a9789b3b6e18fc8f6f9e158006794c046a91
Didi29-04 <alfidianxannov@gmail.com>
2024-11-29 10:59:41 +0700

    Add files via upload

---
+++ app/Http/Controllers/RouteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Route;
+
+class RouteController extends Controller
+{
+    public function index() {
+        return "This is from Controller";
+    }
+}
